4dbb5621503810a1125a27cc2df3e9b70a0ae1f8
Matt Parker <matthew.parker.warren@gmailcom>
2021-12-20 14:48:33 -0600

    updates to read me

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Welcome to your CDK Python project!
+# Welcome to my CDK Python project!
 
 This is a blank project for Python development with CDK.
 
--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,16 +1,22 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_s3 as _s3,
+    core
 )
 from constructs import Construct
 
-class MyFirstCdkProjectStack(Stack):
+class MyFirstCdkProjectStack(core.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketID",
+            bucket_name="myfirstcdkproject73829756",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
 
         # example resource
         # queue = sqs.Queue(
